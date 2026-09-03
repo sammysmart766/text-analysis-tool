f59ded0be0bc76133040686d0fa4bfb617febdd6
Samuel Damilare <sammysmart766@gmail.com>
2026-09-03 14:44:40 +0100

    converted workflows into functions

--- analyze.py
+++ analyze.py
@@ -1,9 +1,16 @@
-print('\nWelcome to the text analysis tool. I will mine and analyze a bodY of text from the file you give me')
+def welcomeUser():
+    print('\nWelcome to the text analysis tool. I will mine and analyze a bodY of text from the file you give me')
 
 # Get username
-
-# Get input from user into the terminal
-username = input('\nTo begin, please enter your username: ')
+def Getusername():
+    # Get input from user into the terminal
+    usernameFromInput = input('\nTo begin, please enter your username: ')
+    return usernameFromInput
 
 # Greet the user
-print('Hello' + ' ' + username)
+def greetuser(name):
+    print('Hello' + ' ' + name)
+    
+welcomeUser()
+username = Getusername()
+greetuser(username)
